369ea84c487f777f132fc6a6c026497b6d0d3aeb
inmny <1342885270@qq.com>
2023-11-08 22:27:29 +0800

    Repo: optimize prepare_debug.ps1

--- prepare_debug.ps1
+++ prepare_debug.ps1
@@ -1,5 +1,5 @@
 $GamePath="C:\Program Files (x86)\Steam\steamapps\common\worldbox\worldbox_Data\StreamingAssets\mods"
-$BuildPath="C:\Users\Inmny\source\repos\NeoModLoader\NeoModLoader\bin\Debug\net481"
+$BuildPath="bin\Debug\net48"
 
 Copy-Item -Path "$BuildPath\NeoModLoader.dll" -Destination "$GamePath\NeoModLoader.dll" -Force
 Copy-Item -Path "$BuildPath\NeoModLoader.pdb" -Destination "$GamePath\NeoModLoader.pdb" -Force
