65a25a291709448c7ec1845ba1adc35058b547fb
key44 <k.minihuber@students.htl-leonding.ac.at>
2023-11-08 19:24:24 +0100

    Feature: made it possible to get access token for account based on successful authentication

--- utils/authentication/DiscordRoleAuthViaUserLoginUtils.cs
+++ utils/authentication/DiscordRoleAuthViaUserLoginUtils.cs
@@ -1,9 +1,20 @@
 using System.Net;
+using System.Text;
 using NeoModLoader.constants;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
 namespace NeoModLoader.utils.authentication;
 
 public class DiscordRoleAuthViaUserLoginUtils
 {
+    private struct TokenInfo
+    {
+        public string access_token;
+        public string token_type;
+        public string expires_in;
+        public string refresh_token;
+        public string scope;
+    }
     private const string client_id = Setting.discord_auth_client_id;
 
     public static bool Authenticate()
@@ -13,20 +24,21 @@ public class DiscordRoleAuthViaUserLoginUtils
 
     public static void Test()
     {
-        GetAuthToken();
+        TokenInfo tokenInfo = GetAuthToken();
+        System.Diagnostics.Debug.WriteLine(tokenInfo.access_token);
     }
-    public static string GetAuthToken()
+    private static TokenInfo GetAuthToken()
     {
         HttpListener listener = new HttpListener();
         listener.Prefixes.Add("http://localhost:36549/");
         listener.Start();
-        
+
         System.Diagnostics.Process.Start("https://discord.com/api/oauth2/authorize?client_id=" + client_id + "&redirect_uri=http%3A%2F%2Flocalhost%3A36549&response_type=code&scope=identify");
         new Task(() =>
         {
             HttpListener listener_ref = listener;
             int waitTime = 0;
-            while(waitTime < 60000)
+            while (waitTime < 60000)
             {
                 if (listener_ref.IsListening)
                 {
@@ -45,29 +57,40 @@ public class DiscordRoleAuthViaUserLoginUtils
         {
             context = listener.GetContext();
         }
-        catch (InvalidOperationException)
+        catch (InvalidOperationException e)
         {
-            return null;
+            throw new Exception("Failed to get context", e);
         }
         HttpListenerRequest request = context.Request;
         HttpListenerResponse response = context.Response;
-        const string response_text = "<html><head><title>NeoModLoader</title></head><body>You can close this page!</body></html>";
-        response.OutputStream.Write(response_text.ToCharArray().Select((c => (byte) c)).ToArray(), 0, response_text.Length);
+        const string response_text = "<html><head><title>NeoModLoader</title><style>body {background-color: black; color: white;}</style></head><body>You can close this page!</body></html>";
+        response.OutputStream.Write(response_text.ToCharArray().Select((c => (byte)c)).ToArray(), 0, response_text.Length);
         response.Close();
         string code = request.QueryString["code"];
         System.Diagnostics.Debug.WriteLine(code);
         listener.Close();
-        /*
-        string res = HttpUtils.Post("https://github.com/login/oauth/access_token", new Dictionary<string, string>()
+        string res = HttpUtils.Post("https://discord.com/api/oauth2/token", new Dictionary<string, string>()
         {
-            { "client_id", client_id },
-            { "client_secret", client_secret },
-            { "code", code }
+            { "code", code },
+            { "grant_type", "authorization_code" },
+            { "redirect_uri", "http://localhost:36549" },
+            { "scope", "identify" }
         }, new Dictionary<string, string>()
         {
-            { "Accept", "application/json" }
+            { "Accept", "application/json" },
+            { "Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(client_id + ":" + "NOT_COMMITING_THIS_THING :3")) }, // TODO: MY GOD PLEASE STORE THIS SECRET MORE SAFELY BEFORE COMMITING
         });
-        */
-        return "";
+        System.Diagnostics.Debug.WriteLine(res);
+        Console.WriteLine(res);
+        string[] resSegments = res.Split(',');
+        TokenInfo result = new TokenInfo
+        {
+            token_type = resSegments[0].Split(':')[1].Trim('"', ' '),
+            access_token = resSegments[1].Split(':')[1].Trim('"', ' '),
+            expires_in = resSegments[2].Split(':')[1].Trim('"', ' '),
+            refresh_token = resSegments[3].Split(':')[1].Trim('"', ' '),
+            scope = resSegments[4].Split(':')[1].Trim('"', ' ', '}')
+        };
+        return result;
     }
 }
