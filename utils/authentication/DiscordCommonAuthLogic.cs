08796b0d66fde26ca83f9b0bcf49751c002b292e
key44 <k.minihuber@students.htl-leonding.ac.at>
2023-11-09 17:15:14 +0100

    Repo: code prep for before Jupe gives me the API endpoint

--- utils/authentication/DiscordCommonAuthLogic.cs
+++ utils/authentication/DiscordCommonAuthLogic.cs
@@ -4,6 +4,8 @@ internal static class DiscordCommonAuthLogic
 {
     internal static IEnumerable<string> GetRolesOfUser(string user_id)
     {
+        HttpResponseMessage res = HttpUtils.Get("URL_OF_JUPE_API" + user_id, new Dictionary<string, string>());
+        // expected response [ a list of strings ], empty if user has no roles, not on server, or ID doesn't exist
         return Array.Empty<string>();
     }
     
