8e0559e8649cda208e32fd936b67d8f05a5ea9c1
key44 <k.minihuber@students.htl-leonding.ac.at>
2023-11-09 23:06:15 +0100

    Feature: added role parsing, the code should now be fully functional whenever Jupe deploys the API endpoint

--- utils/authentication/DiscordCommonAuthLogic.cs
+++ utils/authentication/DiscordCommonAuthLogic.cs
@@ -4,9 +4,10 @@ internal static class DiscordCommonAuthLogic
 {
     internal static IEnumerable<string> GetRolesOfUser(string user_id)
     {
-        HttpResponseMessage res = HttpUtils.Get("URL_OF_JUPE_API" + user_id, new Dictionary<string, string>());
+        HttpResponseMessage res = HttpUtils.Get("https://65.108.87.77:3000/user/roles/" + user_id, new Dictionary<string, string>());
         // expected response [ a list of strings ], empty if user has no roles, not on server, or ID doesn't exist
-        return Array.Empty<string>();
+        string resJson = res.Content.ReadAsStringAsync().Result;
+        return resJson.Trim('[', ']', ' ').Split(',').Select(role => role.Trim('"', ' '));
     }
     
     internal static bool ModderIsInRolesList(IEnumerable<string> roles)
