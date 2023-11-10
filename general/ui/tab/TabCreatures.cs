0db896250e2b3a9ac610571a8e2e4bb8cbf3aff1
inmny <1342885270@qq.com>
2023-11-10 20:33:28 +0800

    Feature: Ready for patching button to vanilla tab

---
+++ general/ui/tab/TabCreatures.cs
@@ -0,0 +1,11 @@
+namespace NeoModLoader.General.UI.Tab;
+
+public static class TabCreatures
+{
+    public const string RACES = "races";
+    public const string LAND_CREATURES = "land_creatures";
+    public const string SEA_CREATURES = "sea_creatures";
+    public const string MAGICAL_CREATURES = "magical_creatures";
+    public const string UNDEAD_CREATURES = "undead_creatures";
+    public const string IMPROPER_CREATURES = "improper_creatures";
+}
---
+++ general/ui/tab/TabMain.cs
@@ -0,0 +1,7 @@
+namespace NeoModLoader.General.UI.Tab;
+
+public static class TabMain
+{
+    public const string WORLD_INFO = "world_info";
+    public const string REBUILD = "rebuild";
+}
