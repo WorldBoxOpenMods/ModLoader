d6275a19a99ee67142613764800c9ca4a5683c78
inmny <1342885270@qq.com>
2023-11-30 16:13:46 +0800

    Feature: New technic for hotfix

--- utils/ModReloadUtils.cs
+++ utils/ModReloadUtils.cs
@@ -1,13 +1,19 @@
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
 using HarmonyLib;
 using HarmonyLib.Tools;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 using MonoMod.Utils;
 using NeoModLoader.api;
 using NeoModLoader.api.attributes;
 using NeoModLoader.constants;
 using NeoModLoader.services;
+using ExceptionHandler = Mono.Cecil.Cil.ExceptionHandler;
+using OpCode = System.Reflection.Emit.OpCode;
+using OpCodes = System.Reflection.Emit.OpCodes;
 
 namespace NeoModLoader.utils;
 
@@ -72,10 +78,19 @@ internal static class ModReloadUtils
             
             try
             {
-                MethodInfo old_method = old_assembly.GetType(new_method.DeclaringType.FullName).GetMethod(new_method.Name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                MethodInfo old_method = old_assembly.GetType(new_method.DeclaringType.FullName).GetMethod(
+                    new_method.Name,
+                    BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic, null,
+                    new_method.Parameters.Select(x => x.ParameterType.ResolveReflection()).ToArray(), null);
                 if (old_method == null)
                 {
                     LogService.LogWarning($"No found method {new_method.DeclaringType.FullName}::{new_method.Name} in old assembly");
+                    continue;
+                }
+
+                if (!NeedHotfix(old_method, new_method))
+                {
+                    LogService.LogInfo($"Method {new_method.Name} does not need hotfix");
                     continue;
                 }
                 LogService.LogInfo($"Hotfixing method {new_method.Name} with following instructions(total {new_method.Body.Instructions.Count}):");
@@ -83,7 +98,7 @@ internal static class ModReloadUtils
             }
             catch (Exception e)
             {
-                LogService.LogError($"Failed to hotfix method {new_method.Name}");
+                LogService.LogError($"Failed to hotfix method {new_method.Name}, Most likely because NeoModLoader does not support such method hotfix now.");
                 LogService.LogError(e.Message);
                 LogService.LogError(e.StackTrace);
                 continue;
@@ -91,6 +106,23 @@ internal static class ModReloadUtils
         }
         assembly_definition.Dispose();
         return true;
+    }
+
+    private static bool NeedHotfix(MethodInfo pOldMethod, MethodDefinition pNewMethod)
+    {
+        var old_il = pOldMethod.GetMethodBody().GetILAsByteArray();
+        var new_il = pNewMethod.ResolveReflection().GetMethodBody().GetILAsByteArray();
+        if (old_il.Length != new_il.Length)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < old_il.Length; i++)
+        {
+            if (old_il[i] != new_il[i]) return true;
+        }
+
+        return false;
     }
 
     private static void InitializeOpcodeMap()
@@ -121,43 +153,247 @@ internal static class ModReloadUtils
 
     private static void HotfixMethod(Harmony pHarmony, MethodDefinition pNewMethod, MethodInfo pOldMethod)
     {
-        _new_method = pNewMethod;
-        pHarmony.Unpatch(pOldMethod, HarmonyPatchType.Transpiler, pHarmony.Id);
-        pHarmony.Patch(pOldMethod, transpiler: new HarmonyMethod(AccessTools.Method(typeof(ModReloadUtils), nameof(il_code_replace_transpiler))));
+        ReplaceMethod(pOldMethod, regenerate(pNewMethod));
+        //pHarmony.Unpatch(pOldMethod, HarmonyPatchType.Transpiler, pHarmony.Id);
+        //pHarmony.Patch(pOldMethod, transpiler: new HarmonyMethod(AccessTools.Method(typeof(ModReloadUtils), nameof(il_code_replace_transpiler))));
     }
-    static MethodDefinition _new_method;
-    static IEnumerable<CodeInstruction> il_code_replace_transpiler(IEnumerable<CodeInstruction> pInstructions)
+    private static void ReplaceMethod(MethodInfo pOldMethod, DynamicMethodDefinition pNewMethod)
     {
-        if(!Config.isEditor)
-        {
-            LogService.LogInfo($"\tHidden. Make it visible by setting {nameof(Config.isEditor)} to true");
-        }
-        List<CodeInstruction> new_instr = new();
-        foreach (var cecil_instr in _new_method.Body.Instructions)
-        {
-            OpCode op_code = _op_code_map[cecil_instr.OpCode];
-            object operand = ConvertCecilOperand(cecil_instr.Operand);
-            if (Config.isEditor)
-            {
-                LogService.LogInfo($"\t{op_code}\t\t {operand}({operand?.GetType().FullName})");
+        RuntimeHelpers.PrepareMethod(pOldMethod.MethodHandle);
+        IntPtr pBody = pOldMethod.MethodHandle.GetFunctionPointer();
+
+        MethodInfo pNewMethodMethod = pNewMethod.Generate();
+        RuntimeHelpers.PrepareMethod(pNewMethodMethod.MethodHandle);
+        IntPtr pBorrowed = pNewMethodMethod.MethodHandle.GetFunctionPointer();
+
+        LogService.LogInfo($"Is 64bit: {Environment.Is64BitProcess}");
+
+        unsafe {
+            var ptr = (byte*)pBody.ToPointer();
+            var ptr2 = (byte*)pBorrowed.ToPointer();
+            var ptrDiff = ptr2 - ptr - 5;
+            if (ptrDiff < (long)0xFFFFFFFF && ptrDiff > (long)-0xFFFFFFFF) {
+                // 32-bit relative jump, available on both 32 and 64 bit arch.
+                LogService.LogInfo($"diff is {ptrDiff} doing relative jmp");
+                LogService.LogInfo(string.Format("patching on {0:X}, target: {1:X}", (ulong)ptr, (ulong)ptr2));
+                *ptr = 0xe9; // JMP
+                *((uint*)(ptr + 1)) = (uint)ptrDiff;
             }
-            new_instr.Add(new CodeInstruction(
-                op_code, operand
-            ));
+            else {
+                LogService.LogInfo($"diff is {ptrDiff} doing push+ret trampoline");
+                LogService.LogInfo(string.Format("patching on {0:X}, target: {1:X}", (ulong)ptr, (ulong)ptr2));
+                if (Environment.Is64BitProcess) {
+                    // For 64bit arch and likely 64bit pointers, do:
+                    // PUSH bits 0 - 32 of addr
+                    // MOV [RSP+4] bits 32 - 64 of addr
+                    // RET
+                    var cursor = ptr;
+                    *(cursor++) = 0x68; // PUSH
+                    *((uint*)cursor) = (uint)ptr2;
+                    cursor += 4;
+                    *(cursor++) = 0xC7; // MOV [RSP+4]
+                    *(cursor++) = 0x44;
+                    *(cursor++) = 0x24;
+                    *(cursor++) = 0x04;
+                    *((uint*)cursor) = (uint)((ulong)ptr2 >> 32);
+                    cursor += 4;
+                    *(cursor++) = 0xc3; // RET
+                }
+                else {
+                    // For 32bit arch and 32bit pointers, do: PUSH addr, RET.
+                    *ptr = 0x68;
+                    *((uint*)(ptr + 1)) = (uint)ptr2;
+                    *(ptr + 5) = 0xC3;
+                }
+            }
+
+            LogService.LogInfo(string.Format("Patched 0x{0:X} to 0x{1:X}.", (ulong)ptr, (ulong)ptr2));
         }
-        return new_instr.AsEnumerable();
     }
 
-    private static object ConvertCecilOperand(object pCecilOperand)
+    static DynamicMethodDefinition regenerate(MethodDefinition pMethodDefinition)
     {
-        if (pCecilOperand == null) return null;
-        if(pCecilOperand is MemberReference method_reference)
+        DynamicMethodDefinition dynamic_method_definition = new DynamicMethodDefinition(pMethodDefinition.Name,
+            pMethodDefinition.ReturnType.ResolveReflection(),
+            pMethodDefinition.Parameters.Select(x => x.ParameterType.ResolveReflection()).ToArray());
+        if (!pMethodDefinition.IsStatic)
         {
-            return method_reference.ResolveReflection();
+            dynamic_method_definition.Definition.Parameters.Insert(0,
+                new ParameterDefinition(pMethodDefinition.DeclaringType));
+        }
+        foreach(var parameter in pMethodDefinition.Parameters)
+        {
+            LogService.LogInfo($"\tDeclare parameter {parameter.ToString()}({parameter.ParameterType.FullName})");
+        }
+        
+        ILGenerator il = dynamic_method_definition.GetILGenerator();
+        if (pMethodDefinition.Body.InitLocals)
+        {
+            dynamic_method_definition.Definition.Body.InitLocals = true;
+        }
+        foreach(var local_var in pMethodDefinition.Body.Variables)
+        {
+            LogService.LogInfo($"\tDeclare local variable {local_var.ToString()}({local_var.VariableType.FullName})");
+            il.DeclareLocal(local_var.VariableType.ResolveReflection());
         }
 
-        return pCecilOperand;
+        var labels = new Dictionary<Instruction, Label>();
+        foreach(var inst in pMethodDefinition.Body.Instructions)
+        {
+            if (inst.Operand != null && inst.Operand is Instruction) {
+                var opinst = (Instruction)(inst.Operand);
+                LogService.LogInfo($"\tDeclare label for {opinst.ToString()}");
+                labels[opinst] = il.DefineLabel();
+            }
+        }
+
+        Dictionary<Instruction, ExceptionHandler> excep_handlers = new();
+        
+        foreach(var excep in pMethodDefinition.Body.ExceptionHandlers)
+        {
+            LogService.LogInfo($"\tDeclare exception handler for {excep.ToString()}");
+            excep_handlers[excep.TryStart] = excep;
+            excep_handlers[excep.TryEnd] = excep;
+            excep_handlers[excep.HandlerStart] = excep;
+            excep_handlers[excep.HandlerEnd] = excep;
+            if (excep.TryStart != null)
+            {
+                //labels[excep.TryStart] = il.BeginExceptionBlock();
+            }
+        }
+
+        try
+        {
+            foreach (var inst in pMethodDefinition.Body.Instructions)
+            {
+                if (labels.TryGetValue(inst, out var label))
+                {
+                    il.MarkLabel(label);
+                }
+
+                if (excep_handlers.TryGetValue(inst, out var excep_handler))
+                {
+                    if (inst == excep_handler.TryEnd)
+                    {
+                        LogService.LogWarning("TryEnd");
+                        //il.EndExceptionBlock();
+                    }
+                    else if (inst == excep_handler.HandlerStart)
+                    {
+                        LogService.LogWarning("HandlerStart");
+                        /*
+                        switch (excep_handler.HandlerType)
+                        {
+                            case ExceptionHandlerType.Catch:
+                                il.BeginCatchBlock(excep_handler.CatchType.ResolveReflection());
+                                break;
+                            case ExceptionHandlerType.Filter:
+                                il.BeginExceptFilterBlock();
+                                break;
+                            case ExceptionHandlerType.Finally:
+                                il.BeginFinallyBlock();
+                                break;
+                            case ExceptionHandlerType.Fault:
+                                il.BeginFaultBlock();
+                                break;
+                        }
+                        continue;*/
+                    }
+                    else if (inst == excep_handler.HandlerEnd)
+                    {
+                        LogService.LogWarning("HandlerEnd");
+                        //il.EndExceptionBlock();
+                    }
+                    else
+                    {
+                        // TryStart
+                        //il.MarkLabel(il.BeginExceptionBlock());
+                        LogService.LogWarning("TryStart");
+                    }
+                    
+                }
+                var op_code = _op_code_map[inst.OpCode];
+                
+                if (op_code == OpCodes.Endfinally) continue;
+                
+                LogService.LogInfo($"\t{op_code}\t\t {inst.Operand}({inst.Operand?.GetType().FullName})");
+
+                if (inst.Operand == null)
+                {
+                    il.Emit(op_code);
+                    continue;
+                }
+
+                if (inst.Operand is Instruction)
+                {
+                    il.Emit(op_code, labels[(Instruction)inst.Operand]);
+                    continue;
+                }
+
+                var operand_type = inst.Operand.GetType();
+
+
+                if (inst.Operand is MemberReference member_reference)
+                {
+                    operand_type = member_reference.ResolveReflection().GetType();
+                    if (!_emit_method_cache.TryGetValue(operand_type, out var emit_method))
+                    {
+                        emit_method = AccessTools.Method(typeof(ILGenerator), "Emit",
+                            new Type[] { typeof(OpCode), operand_type });
+                        _emit_method_cache[operand_type] = emit_method;
+                    }
+
+                    if (emit_method == null)
+                    {
+                        throw new Exception($"Failed to get emit method for {operand_type.FullName}");
+                    }
+
+                    emit_method.Invoke(il, new object[] { op_code, member_reference.ResolveReflection() });
+                }
+                else if (inst.Operand is VariableReference variable_reference)
+                {
+                    il.Emit(op_code, variable_reference.Index);
+                }
+                else
+                {
+                    if (!_emit_method_cache.TryGetValue(operand_type, out var emit_method))
+                    {
+                        emit_method = AccessTools.Method(typeof(ILGenerator), "Emit",
+                            new Type[] { typeof(OpCode), operand_type });
+                        _emit_method_cache[operand_type] = emit_method;
+                    }
+
+                    if (emit_method == null)
+                    {
+                        throw new Exception($"Failed to get emit method for {operand_type.FullName}");
+                    }
+
+                    emit_method.Invoke(il, new object[] { op_code, inst.Operand });
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            LogService.LogError(e.Message);
+            LogService.LogError(e.StackTrace);
+        }
+        finally
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Current instructions:");
+            foreach (var inst in dynamic_method_definition.GetILProcessor().Body.Instructions)
+            {
+                sb.AppendLine($"\t{inst.OpCode}\t\t {inst.Operand}({inst.Operand?.GetType().FullName})");
+            }
+
+            LogService.LogWarning(sb.ToString());
+        }
+        
+        return dynamic_method_definition;
     }
+
+    private static Dictionary<Type, MethodInfo> _emit_method_cache = new();
+
     public static bool Reload()
     {
         try
