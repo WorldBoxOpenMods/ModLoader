a4c22f785e97b337ffb9476f32dbcf3062bf4d7f
inmny <1342885270@qq.com>
2023-12-21 20:07:04 +0800

    Bugfix: Support add null PowerButton to wrapped tab

--- general/ui/tab/WrappedPowersTab.cs
+++ general/ui/tab/WrappedPowersTab.cs
@@ -1,91 +1,26 @@
 using System.Runtime.CompilerServices;
-using NeoModLoader.services;
 using UnityEngine;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.UI;
 
 namespace NeoModLoader.General.UI.Tab;
 
 internal class WrappedPowersTab
 {
-    private class WrappedRectTransform
-    {
-        public readonly RectTransform Rect;
-        public readonly Vector2 PositionInGroup;
-        public readonly bool Placehold;
-        public WrappedRectTransform(RectTransform pRect, Vector2 pPositionInGroup, bool pPlacehold)
-        {
-            Rect = pRect;
-            PositionInGroup = pPositionInGroup;
-            Placehold = pPlacehold;
-        }
-    }
-    private class PlaceholdRegions
-    {
-        private class SimpleRegion
-        {
-            public readonly Vector2 LeftUpCorner;
-            public readonly Vector2 RightDownCorner;
-
-            public SimpleRegion(RectTransform pRect)
-            {
-                var rect = pRect.rect;
-                LeftUpCorner = new Vector2(rect.xMin, rect.yMax);
-                RightDownCorner = new Vector2(rect.xMax, rect.yMin);
-            }
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool Contains(float pX, float pY)
-            {
-                return ContainsX(pX) && ContainsY(pY);
-            }
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool ContainsX(float pX)
-            {
-                return pX >= LeftUpCorner.x && pX <= RightDownCorner.x;
-            }
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool ContainsY(float pY)
-            {
-                return pY >= RightDownCorner.y && pY <= RightDownCorner.y;
-            }
-        }
-
-        private HashSet<SimpleRegion> _regions = new();
-        public void AddRegion(RectTransform pRect)
-        {
-            _regions.Add(new SimpleRegion(pRect));
-        }
-
-        public bool Overlap(RectTransform pRect)
-        {
-            var rect = pRect.rect;
-            foreach (SimpleRegion region in _regions)
-            {
-                if (region.Contains(rect.xMin, rect.yMin)
-                    || region.Contains(rect.xMin, rect.yMax)
-                    || region.Contains(rect.xMax, rect.yMin)
-                    || region.Contains(rect.xMax, rect.yMax))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-    public PowersTab Tab;
-    public bool Modifiable;
-    private Dictionary<string, List<PowerButton>> ButtonGroups;
-    private Dictionary<string, List<WrappedRectTransform>> CustomRectGroups;
-    private Queue<GameObject> _inactive_lines;
-    private Queue<GameObject> _active_lines;
+    private const float space = 4;
+    private const float tab_start_x = 87.4f;
+    private const float assumed_button_size = 32;
 
     private static readonly RectTransform _empty_button_placehold =
         new GameObject("Empty Button Placehold", typeof(RectTransform)).GetComponent<RectTransform>();
 
-    public static void _init()
-    {
-        _empty_button_placehold.SetParent(WorldBoxMod.Transform);
-    }
+    private static readonly float[] available_y = new float[2] { 18, -18 };
+    private Queue<GameObject> _active_lines;
+    private Queue<GameObject> _inactive_lines;
+    private Dictionary<string, List<PowerButton>> ButtonGroups;
+    private Dictionary<string, List<WrappedRectTransform>> CustomRectGroups;
+    public bool Modifiable;
+    public PowersTab Tab;
+
     public WrappedPowersTab(PowersTab pPowersTab)
     {
         Tab = pPowersTab;
@@ -101,44 +36,50 @@ internal class WrappedPowersTab
         _active_lines = new();
     }
 
+    public static void _init()
+    {
+        _empty_button_placehold.SetParent(WorldBoxMod.Transform);
+    }
+
     public bool HasGroup(string pGroupId)
     {
         return ButtonGroups.ContainsKey(pGroupId);
     }
+
     public void AddPowerButton(string pGroupId, PowerButton pPowerButton)
     {
         var group = ButtonGroups[pGroupId];
 
-        Transform transform;
-        (transform = pPowerButton.transform).SetParent(Tab.transform);
-        transform.localScale = Vector3.one;
-        
+        if (pPowerButton != null)
+        {
+            Transform transform;
+            (transform = pPowerButton.transform).SetParent(Tab.transform);
+            transform.localScale = Vector3.one;
+        }
+
         group.Add(pPowerButton);
     }
 
     public void AddCustomRect(string pGroupId, RectTransform pRect, Vector2 pPositionInGroup, bool pPlacehold)
     {
         var group = CustomRectGroups[pGroupId];
-        
+
         pRect.SetParent(Tab.transform);
         pRect.localScale = Vector3.one;
-        
+
         group.Add(new(pRect, pPositionInGroup, pPlacehold));
     }
+
     public void AddGroup(string pGroupId)
     {
         ButtonGroups.Add(pGroupId, new());
         CustomRectGroups.Add(pGroupId, new());
     }
+
     public void ResetGroups()
     {
         ButtonGroups.Clear();
     }
-
-    private const float space = 4;
-    private const float tab_start_x = 87.4f;
-    private const float assumed_button_size = 32;
-    private static readonly float[] available_y = new float[2] { 18, -18 };
 
     public void UpdateLayout()
     {
@@ -158,12 +99,13 @@ internal class WrappedPowersTab
             if (button_group.Count > 0 || rect_group.Count > 0)
             {
                 group_start_x += space * 2;
-                if(!first_line)
+                if (!first_line)
                     _add_line(group_start_x);
                 else
                 {
                     first_line = false;
                 }
+
                 group_start_x += space * 2;
             }
             else
@@ -227,7 +169,7 @@ internal class WrappedPowersTab
                 group_start_x += assumed_button_size / 2;
             }
         }
-        
+
         Tab.powerButtons.Clear();
         foreach (PowerButton power_button in Tab.GetComponentsInChildren<PowerButton>())
         {
@@ -236,6 +178,7 @@ internal class WrappedPowersTab
                 Tab.powerButtons.Add(power_button);
             }
         }
+
         foreach (PowerButton power_button in Tab.powerButtons)
         {
             power_button.findNeighbours(Tab.powerButtons);
@@ -255,8 +198,81 @@ internal class WrappedPowersTab
             line.GetComponent<Image>().enabled = true;
             line.transform.localScale = new(1, 48.3f, 1);
         }
+
         line.SetActive(true);
         line.transform.localPosition = new(pX, 37.2f);
         _active_lines.Enqueue(line);
+    }
+
+    private class WrappedRectTransform
+    {
+        public readonly bool Placehold;
+        public readonly Vector2 PositionInGroup;
+        public readonly RectTransform Rect;
+
+        public WrappedRectTransform(RectTransform pRect, Vector2 pPositionInGroup, bool pPlacehold)
+        {
+            Rect = pRect;
+            PositionInGroup = pPositionInGroup;
+            Placehold = pPlacehold;
+        }
+    }
+
+    private class PlaceholdRegions
+    {
+        private HashSet<SimpleRegion> _regions = new();
+
+        public void AddRegion(RectTransform pRect)
+        {
+            _regions.Add(new SimpleRegion(pRect));
+        }
+
+        public bool Overlap(RectTransform pRect)
+        {
+            var rect = pRect.rect;
+            foreach (SimpleRegion region in _regions)
+            {
+                if (region.Contains(rect.xMin, rect.yMin)
+                    || region.Contains(rect.xMin, rect.yMax)
+                    || region.Contains(rect.xMax, rect.yMin)
+                    || region.Contains(rect.xMax, rect.yMax))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private class SimpleRegion
+        {
+            public readonly Vector2 LeftUpCorner;
+            public readonly Vector2 RightDownCorner;
+
+            public SimpleRegion(RectTransform pRect)
+            {
+                var rect = pRect.rect;
+                LeftUpCorner = new Vector2(rect.xMin, rect.yMax);
+                RightDownCorner = new Vector2(rect.xMax, rect.yMin);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Contains(float pX, float pY)
+            {
+                return ContainsX(pX) && ContainsY(pY);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool ContainsX(float pX)
+            {
+                return pX >= LeftUpCorner.x && pX <= RightDownCorner.x;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool ContainsY(float pY)
+            {
+                return pY >= RightDownCorner.y && pY <= RightDownCorner.y;
+            }
+        }
     }
 }
