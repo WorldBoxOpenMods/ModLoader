82788242e6216d419963a1ebe3c6bc74cb8680c8
inmny <1342885270@qq.com>
2023-12-27 13:05:33 +0800

    Optim: Better stack trace log

--- utils/OtherUtils.cs
+++ utils/OtherUtils.cs
@@ -1,34 +1,47 @@
-using NeoModLoader.services;
 using System.Diagnostics;
 using System.Text;
 
 namespace NeoModLoader.utils;
+
 /// <summary>
 /// It provides some uncommon methods
 /// </summary>
 public static class OtherUtils
 {
     /// <summary>
-    /// Get StackTrace from this method, and skip the first <paramref name="skipFrames"/> frames
+    /// Get StackTrace from this method, and skip the first <paramref name="skip_frames"/> frames
     /// </summary>
-    /// <param name="skipFrames">The frame number to skip</param>
+    /// <param name="skip_frames">The frame number to skip</param>
     /// <param name="indent">Each line start with several indent character</param>
-    public static string GetStackTrace(int skipFrames = 0, char indent = '\t')
+    public static string GetStackTrace(int skip_frames = 0, string indent = "")
     {
+        string stack_trace = Environment.StackTrace;
         StringBuilder sb = new StringBuilder();
-        var stackTrace = new System.Diagnostics.StackTrace();
-        StackFrame[] frames = stackTrace.GetFrames();
-        if (frames == null)
+
+        string[] lines = stack_trace.Split('\n');
+        if (!string.IsNullOrEmpty(indent))
         {
-            return "";
+            for (int i = skip_frames; i < lines.Length; i++)
+            {
+                sb.AppendLine(lines[i]);
+            }
         }
-        for(int i = skipFrames; i < frames.Length; i++)
+        else
         {
-            sb.Append(new string(indent, i - skipFrames));
-            sb.AppendLine(frames[i].ToString());
+            for (int i = skip_frames; i < lines.Length; i++)
+            {
+                for (int j = 0; j < i - skip_frames; j++)
+                {
+                    sb.Append(indent);
+                }
+
+                sb.AppendLine(lines[i]);
+            }
         }
+
         return sb.ToString();
     }
+
     /// <summary>
     /// Search the stack trace to see if the method is called by <paramref name="pMethodName"/> in <paramref name="pTypeConstraint"/>
     /// </summary>
@@ -37,27 +50,33 @@ public static class OtherUtils
     /// <param name="pSearchAll">Wheather search the entire stack trace</param>
     public static bool CalledBy(string pMethodName, Type pTypeConstraint, bool pSearchAll = false)
     {
-        var stackTrace = new System.Diagnostics.StackTrace();
+        var stackTrace = new StackTrace();
         StackFrame[] frames = stackTrace.GetFrames();
         if (frames == null)
         {
             return false;
         }
+
         if (frames.Length < 3)
         {
             return false;
         }
+
         if (!pSearchAll)
         {
-            return frames[2].GetMethod().Name == pMethodName && (frames[2].GetType() == pTypeConstraint || frames[2].GetType().IsSubclassOf(pTypeConstraint));
+            return frames[2].GetMethod().Name == pMethodName && (frames[2].GetType() == pTypeConstraint ||
+                                                                 frames[2].GetType().IsSubclassOf(pTypeConstraint));
         }
-        for(int i = 2; i < frames.Length; i++)
+
+        for (int i = 2; i < frames.Length; i++)
         {
-            if (frames[i].GetMethod().Name == pMethodName && (frames[i].GetType() == pTypeConstraint || frames[i].GetType().IsSubclassOf(pTypeConstraint)))
+            if (frames[i].GetMethod().Name == pMethodName && (frames[i].GetType() == pTypeConstraint ||
+                                                              frames[i].GetType().IsSubclassOf(pTypeConstraint)))
             {
                 return true;
             }
         }
+
         return false;
     }
 }
