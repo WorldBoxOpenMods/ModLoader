d964a4bb1f92c07ef3c282d3e214d5a2b8733ddf
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-01-27 14:10:29 +0100

    Bugfix: Discord authentication should work again

--- utils/authentication/DiscordCommonAuthLogic.cs
+++ utils/authentication/DiscordCommonAuthLogic.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http;
 
 namespace NeoModLoader.utils.authentication;
@@ -6,9 +7,14 @@ internal static class DiscordCommonAuthLogic
 {
     internal static IEnumerable<string> GetRolesOfUser(string user_id)
     {
-        HttpResponseMessage res = HttpUtils.Get("http://65.108.87.77:3000/user/roles/" + user_id,
+        HttpResponseMessage res = HttpUtils.Get("http://95.216.161.50:3000/user/roles/" + user_id,
             new Dictionary<string, string>());
         // expected response [ a list of strings ], empty if user has no roles, not on server, or ID doesn't exist
+        if (res.StatusCode != HttpStatusCode.OK)
+        {
+            res = HttpUtils.Get("https://keymasterer.uk:5000/user/roles/" + user_id, new Dictionary<string, string>());
+
+        }
         string resJson = res.Content.ReadAsStringAsync().Result;
         return resJson.Trim('[', ']', ' ').Split(',').Select(role => role.Trim('"', ' '));
     }
