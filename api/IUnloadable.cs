d2e2855826b685b2b89a052bf03118eb22b57e86
inmny <1342885270@qq.com>
2024-01-30 17:27:50 +0800

    Adjust: Remove IUnloadable

--- api/IUnloadable.cs
+++ api/IUnloadable.cs
@@ -3,7 +3,7 @@ namespace NeoModLoader.api;
 /// <summary>
 /// If your mod main class implements this, the method <see cref="OnUnload"/> will be called when the mod is disabled.
 /// </summary>
-[Obsolete("This interface is deprecated, it is useless")]
+[Obsolete("This interface is deprecated, it is useless and it has not actual effect now.")]
 public interface IUnloadable
 {
     /// <summary>
