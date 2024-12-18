bba448f8bb80c52ffaa6e641f5b428fd9c16a8c0
inmny <1342885270@qq.com>
2024-12-18 23:09:48 +0800

    Bugfix & Feature: Fix recursive operator "this", and add IEnumerable for PriorityQueue

--- utils/PriorityQueue.cs
+++ utils/PriorityQueue.cs
@@ -1,30 +1,30 @@
+using System.Collections;
+
 namespace NeoModLoader.utils;
 
 /// <summary>
-/// A simple PriorityQueue implementation with binary heap, not thread-safe
+///     A simple PriorityQueue implementation with binary heap, not thread-safe
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class PriorityQueue<T>
+public class PriorityQueue<T> : IEnumerable<T>
 {
-    private IComparer<T> comparer;
-    private T[] heap;
-    private int size;
+    private readonly IComparer<T> comparer;
+    private          T[]          heap;
 
     /// <summary>
-    /// 
     /// </summary>
     /// <param name="capacity"></param>
     /// <param name="comparer"></param>
     public PriorityQueue(int capacity, IComparer<T> comparer)
     {
         this.comparer = comparer;
-        this.heap = new T[capacity > 0 ? capacity : 8];
+        heap = new T[capacity > 0 ? capacity : 8];
     }
 
     /// <summary>
-    /// Current size of the PriorityQueue
+    ///     Current size of the PriorityQueue
     /// </summary>
-    public int Count => size;
+    public int Count { get; private set; }
 
     /// <summary>
     ///     Get value
@@ -35,13 +35,24 @@ public class PriorityQueue<T>
     {
         get
         {
-            if (index > size || index < 0)
-            {
-                throw new IndexOutOfRangeException($"{index} / {size}");
-            }
+            if (index > Count || index < 0) throw new IndexOutOfRangeException($"{index} / {Count}");
 
-            return this[index];
+            return heap[index];
         }
+    }
+
+    /// <summary>
+    ///     Get the enumerator of the PriorityQueue
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator<T> GetEnumerator()
+    {
+        return (IEnumerator<T>)(heap.GetEnumerator() as IEnumerator<T> ?? Array.Empty<T>().GetEnumerator());
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 
     private static int Parent(int i)
@@ -55,34 +66,28 @@ public class PriorityQueue<T>
     }
 
     /// <summary>
-    /// View the top element of the PriorityQueue
+    ///     View the top element of the PriorityQueue
     /// </summary>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException"></exception>
     public T Peek()
     {
-        if (size == 0)
-        {
-            throw new InvalidOperationException("PriorityQueue is empty");
-        }
+        if (Count == 0) throw new InvalidOperationException("PriorityQueue is empty");
 
         return heap[0];
     }
 
     /// <summary>
-    /// Enqueue an element into the PriorityQueue
+    ///     Enqueue an element into the PriorityQueue
     /// </summary>
     /// <param name="x"></param>
     public int Enqueue(T x)
     {
-        if (size == heap.Length)
-        {
-            Array.Resize(ref heap, size << 1);
-        }
+        if (Count == heap.Length) Array.Resize(ref heap, Count << 1);
 
-        size++;
-        heap[size - 1] = x;
-        return SiftUp(size - 1);
+        Count++;
+        heap[Count - 1] = x;
+        return SiftUp(Count - 1);
     }
 
     private int SiftUp(int i)
@@ -90,11 +95,8 @@ public class PriorityQueue<T>
         T x = heap[i];
         while (i > 0)
         {
-            int p = Parent(i);
-            if (comparer.Compare(x, heap[p]) >= 0)
-            {
-                break;
-            }
+            var p = Parent(i);
+            if (comparer.Compare(x, heap[p]) >= 0) break;
 
             heap[i] = heap[p];
             i = p;
@@ -105,24 +107,18 @@ public class PriorityQueue<T>
     }
 
     /// <summary>
-    /// Dequeue an element from the PriorityQueue
+    ///     Dequeue an element from the PriorityQueue
     /// </summary>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException"></exception>
     public T Dequeue()
     {
-        if (size == 0)
-        {
-            throw new InvalidOperationException("PriorityQueue is empty");
-        }
+        if (Count == 0) throw new InvalidOperationException("PriorityQueue is empty");
 
         T x = heap[0];
-        T y = heap[size - 1];
-        size--;
-        if (size != 0)
-        {
-            SiftDown(0, y);
-        }
+        T y = heap[Count - 1];
+        Count--;
+        if (Count != 0) SiftDown(0, y);
 
         return x;
     }
@@ -131,18 +127,12 @@ public class PriorityQueue<T>
     {
         while (true)
         {
-            int l = Left(i);
-            if (l > size - 1)
-            {
-                break;
-            }
+            var l = Left(i);
+            if (l > Count - 1) break;
 
-            int r = l + 1;
-            int c = (r > size - 1 || comparer.Compare(heap[l], heap[r]) <= 0) ? l : r;
-            if (comparer.Compare(x, heap[c]) <= 0)
-            {
-                break;
-            }
+            var r = l + 1;
+            var c = r > Count - 1 || comparer.Compare(heap[l], heap[r]) <= 0 ? l : r;
+            if (comparer.Compare(x, heap[c]) <= 0) break;
 
             heap[i] = heap[c];
             i = c;
