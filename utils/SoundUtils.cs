b31b70cfdf2c927aabed20bd90bb6fa5bc873604
MelvinShwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2025-03-08 09:52:47 +0200

    Update SoundUtils.cs

--- utils/SoundUtils.cs
+++ utils/SoundUtils.cs
@@ -3,13 +3,6 @@ using FMODUnity;
 using HarmonyLib;
 using NeoModLoader.services;
 using Newtonsoft.Json;
-using SimpleJSON;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace NeoModLoader.utils;
